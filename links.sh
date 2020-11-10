c628e3c2485a8e790abe9d0c2bb790ecfebebf82
Raja <craja26@gmail.com>
2020-11-10 16:37:12 +0100

    Update links.sh

--- links.sh
+++ links.sh
@@ -3,3 +3,6 @@ https://www.kimoby.com/blog/how-to-restore-a-large-mysql-database-replica
 
 # Restore single Maria database from Mariabackup.
 https://mariadb.com/resources/blog/how-to-restore-a-single-database-from-mariadb-backup/
+
+# MySQL upgrade from 5.7 to 8.0
+https://lefred.be/content/how-to-safely-upgrade-to-mysql-8-0/

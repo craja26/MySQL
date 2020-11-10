b9bd8de058f26b82647da98018b45a56c5259e5a
Raja <craja26@gmail.com>
2020-11-10 16:29:54 +0100

    Create mysql_inplace_upgrade.sh

---
+++ mysql_inplace_upgrade.sh
@@ -0,0 +1,13 @@
+
+MySQL upgrade from 5.7 to 8.0 steps:
+------------------------------------
+Note: take full backup before starting upgrade steps.
+
+mysqlcheck -u rchikkala -p --all-databases --check-upgrade
+
+wget https://dev.mysql.com/get/Downloads/MySQL-Shell/mysql-shell-8.0.22-1.el7.x86_64.rpm
+
+rpm -ivh mysql-shell-8.0.22-1.el7.x86_64.rpm
+
+yum update --enablerepo=mysql80-community mysql-community-server
+

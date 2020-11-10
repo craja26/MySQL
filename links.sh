fffaf12dcc74b29e6bfc1c84f263a91dae6d1484
Raja <craja26@gmail.com>
2020-11-10 16:33:43 +0100

    Update and rename links.info to links.sh

---
+++ links.sh
@@ -0,0 +1,5 @@
+# Backup and Restore using xtrabackup xbstream.
+https://www.kimoby.com/blog/how-to-restore-a-large-mysql-database-replica
+
+# Restore single Maria database from Mariabackup.
+https://mariadb.com/resources/blog/how-to-restore-a-single-database-from-mariadb-backup/
